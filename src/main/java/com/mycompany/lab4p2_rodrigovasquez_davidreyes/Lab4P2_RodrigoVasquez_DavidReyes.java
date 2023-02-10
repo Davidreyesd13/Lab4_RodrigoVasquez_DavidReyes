eb51f779eb7f6164d1d7ea57057374d0b81de299
davidreyesd13 <davidreyesd13@gmail.com>
2023-02-10 13:28:26 -0600

    Clone

---
+++ src/main/java/com/mycompany/lab4p2_rodrigovasquez_davidreyes/Lab4P2_RodrigoVasquez_DavidReyes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab4p2_rodrigovasquez_davidreyes;
+
+/**
+ *
+ * @author david
+ */
+public class Lab4P2_RodrigoVasquez_DavidReyes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
