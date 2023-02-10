21bf24541c06aeed08a17a0c2f0ce51f7eb31a10
Rodrigo Vasquez <rodrigovasquezcabezas@gmail.com>
2023-02-10 13:53:15 -0600

    Creación de clases

---
+++ src/main/java/com/mycompany/lab4p2_rodrigovasquez_davidreyes/Rey.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab4p2_rodrigovasquez_davidreyes;
+
+/**
+ *
+ * @author Rui
+ */
+public class Rey {
+    
+}
